7e051efdc2fc66030d578cd5018b05379defa6d6
Ubuntu <ubuntu@ip-172-31-39-3.us-west-2.compute.internal>
2016-01-14 08:10:00 +0000

    learn bootstramp

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 import os
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    files = '/<h1/> fjc/<//h1/>'
-    return render_template("main.html", content=files)
+    files = '<h1> fjc</h1>'
+    return render_template("me.html", content=files)
 	
 if __name__ == "__main__":
     app.run()
