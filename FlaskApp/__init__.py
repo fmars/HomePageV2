7abc8a90bf0075a8f6b611373c6ba8a133e5d47e
fmasr <fmarsf@gmail.com>
2016-01-22 09:09:15 +0000

    add message board

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, Markup
+from flask import request, redirect
 from werkzeug.routing import BaseConverter
 import os
+import time
 import logging
-
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -18,23 +19,71 @@ app.url_map.converters['regex'] = RegexConverter
 @app.route("/")
 @app.route("/me")
 def render_homepage_me():
+    app.logger.debug("me rendered")
     return render_template("HomepageMe.html")
 	
 @app.route("/write")
 def render_homepage_write():
+    app.logger.debug("write rendered")
     path = "/var/www/HomePageV2/FlaskApp/static/write"
     file_tree = read_file_tree(path)
-    app.logger.debug(file_tree)
     return render_template("HomepageWrite.html", file_tree = file_tree)
     
 @app.route("/read")
 def render_homepage_read():
+    app.logger.debug("read rendered")
     return render_template("HomepageRead.html")    
+   
+@app.route("/contact")
+def render_homepage_contact():
+    app.logger.debug("contact rendered")
+    comments = get_comments()
+    return render_template("HomepageContact.html", comments=comments)
+    
+@app.route("/comment", methods = ["POST"])
+def comment():
+    app.logger.debug("comment rendered")
+    name = request.form["name"]
+    if not name:
+        name = "anonymous"
+    content = request.form["content"]
+    store_comment(name, content)
+    return redirect("contact")
 
 app.debug = True
     
 if __name__ == "__main__":
     app.run(debug=True)
+    
+def get_comments():
+    delimeter = "###"
+    db_path = "/var/www/HomePageV2/FlaskApp/static/comments/comments"
+    data = ""
+    with open(db_path, "r") as file:
+        data = file.read()
+    
+    parts = data.split(delimeter)
+    app.logger.debug(parts)
+    comments = []
+    for part in parts:
+        if len(part.split("@@@")) != 3:
+            continue
+        [name, content, date] = part.split("@@@")
+        pair = dict(name=name, content=content, date=date)
+        comments.append(pair)
+    app.logger.debug(comments)
+    return comments
+    
+def store_comment(name, content):
+    date = time.strftime("%x")
+    data = "@@@".join([name, content, date])
+    delimeter = "###"
+    db_path = "/var/www/HomePageV2/FlaskApp/static/comments/comments"
+    with open(db_path, "a") as file:
+        if os.stat(db_path).st_size != 0:
+            file.write(delimeter)
+        file.write(data)
+
     
 def read_file_tree(path):
     fileName = path
